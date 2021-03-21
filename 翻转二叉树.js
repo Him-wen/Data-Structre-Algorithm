4cee3573b949c2e0e90cf183925e5e9a62575ccb
Himwen_ <wenvoi@163.com>
2021-03-21 22:03:19 +0800

    Create 翻转二叉树.js

---
+++ 翻转二叉树.js
@@ -0,0 +1,9 @@
+https://leetcode-cn.com/problems/invert-binary-tree/
+var invertTree = function(root) {
+    if(!root)return null;
+    let left = invertTree(root.left)
+    let right = invertTree(root.right)
+    root.left=right
+    root.right=left
+    return root;
+};
