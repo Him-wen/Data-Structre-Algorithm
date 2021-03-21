4da4a3ce2ed78fbb997d6a26ba9fb2740f258127
Himwen_ <wenvoi@163.com>
2021-03-21 22:25:00 +0800

    Create 最大连续1的个数.js

---
+++ 最大连续1的个数.js
@@ -0,0 +1,14 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let maxcount = 0,count = 0;
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]==1){
+            count++;
+        }else{
+        maxcount=Math.max(maxcount,count);
+        count=0;
+        }
+    }
+    maxcount=Math.max(maxcount,count);
+    return maxcount;
+
+};
