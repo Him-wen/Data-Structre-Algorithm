1417257ac44344199ed830888167a4bbddfc4f21
Himwen_ <wenvoi@163.com>
2021-04-07 21:43:40 +0800

    Update #101 对称二叉树.js

--- 二叉树/#101 对称二叉树.js
+++ 二叉树/#101 对称二叉树.js
@@ -7,7 +7,5 @@ var isSymmetric = function(root) {
 let dfs = function(p,q){
     if(!q && !p)return true;
     if(!q || !p || p.val !== q.val)return false;
-    // if(dfs(p.left,q.right) && dfs(q.left,p.right))return true;
-    // return false;
     return dfs(p.left,q.right) && dfs(q.left,p.right);
 }
